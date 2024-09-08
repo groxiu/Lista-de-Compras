08b62046fba8b702a03a98a8f65d2dcf1a41675e
João Melo <119711544+groxiu@users.noreply.github.com>
2024-09-08 13:55:00 -0300

    Lista2.0
    Adicionado a opção do usuário digitar o valor do item. O valor de todos os itens somados  será exibido na tela. 
    Além disso corrigi alguns bugs.

--- script.js
+++ script.js
@@ -1,61 +1,69 @@
-const adicionarTarefa = document.querySelector('.add_compra')
-const textArea = document.querySelector('.formulario')
+const btnCompra = document.querySelector('.add_compra')
+const itemNome = document.querySelector('.input-item')
+const formulario = document.querySelector('.formulario')
+const itemValor = document.querySelector('.input-valor')
+const exibirValorTotal= document.querySelector('.valor-total')
+
+
+
+
 const compra =''
 
 const ulCompra = document.querySelector('.lista')
 
+
 let listaDeCompras = JSON.parse(localStorage.getItem('item')) || []
 
 
-adicionarTarefa.addEventListener('click',()=>{
+calcularTotal()
 
-    const verificar = textArea.value.trim()
-    if (verificar === '') {
-        alert('Você deve preencher o campo')
-        
-    } else {
-        
-        adicionar(compra) 
-        
-       
-    }
-})
-textArea.addEventListener('keydown', (event) => {
-    
-    if (event.key === 'Enter') {
-       
-        adicionar(compra);
-    }
-});
 
 
 
 function criarCompraNaTela(compra){
-    const li = document.createElement('li')
-    li.classList.add('list_item')
-
-    const p =document.createElement('p')
-    p.textContent = compra
-
-    const bt = document.createElement('button')
-    const imgBt = document.createElement('img')
-    imgBt.setAttribute('src', '/img/dell.png')
-    bt.append(imgBt)
 
 
-    li.append(p)
-    li.append(bt)
-    ulCompra.append(li)
+        const li = document.createElement('li')
+        li.classList.add('list_item')
 
-    bt.addEventListener('click',(e)=>{
-        li.remove()
-        listaDeCompras.splice(e, 1)
-        atualizarLista()
-        
-    })
-}
+
+        const div = document.createElement('div')
+        div.classList.add('valor-nome')
+
+        const p =document.createElement('p')
+        p.innerHTML=`${compra.nome}`
+        const pValor =document.createElement('p')
+        pValor.innerHTML=`R$${compra.valor}`
+    
+        const bt = document.createElement('button')
+        bt.classList.add('dell-compra')
+        const imgBt = document.createElement('img')
+        imgBt.setAttribute('src', '/img/dell.png')
+        bt.append(imgBt)
+    
+        div.append(bt)
+        div.append(pValor)
+        li.append(div)
+        li.append(p)
+   
+   
+        ulCompra.append(li)
+    
+        bt.addEventListener('click',(e)=>{
+            li.remove()
+            listaDeCompras.splice(e, 1)
+            atualizarLista()
+            
+        })
+    }
+       
+
+   
+
+    
+  
+
      
-         
 
 
 listaDeCompras.forEach(compra => {
@@ -66,15 +74,70 @@ listaDeCompras.forEach(compra => {
 
 function atualizarLista(){
     localStorage.setItem('item', JSON.stringify(listaDeCompras))
+    calcularTotal()
+ 
 }
 
 function adicionar(compra){
-    compra = textArea.value
-    console.log(compra);
-    listaDeCompras.push(compra)
-    console.log(listaDeCompras)
-    criarCompraNaTela(compra)
-    atualizarLista()
 
-    textArea.value = ''
+    compra ={
+        nome: itemNome.value,
+        valor:itemValor.value,
+    }
+
+        listaDeCompras.push(compra)
+        criarCompraNaTela(compra)
+        atualizarLista()
+        calcularTotal()
+
+        itemNome.value = ''
+        itemValor.value = ''
+
+    
+    
+}
+
+formulario.addEventListener('submit', (event) => {
+
+    if (itemNome.value === '' || itemValor.value === '') {
+        alert('Preencha os campos corretamente');
+        event.preventDefault();
+    }
+
+    if (itemValor.value < 0) {
+        alert('Valor indevido');
+        event.preventDefault();
+        return; 
+    }
+
+    event.preventDefault()
+    adicionar(); 
+});
+
+
+formulario.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        if (itemNome.value === '' || itemValor.value === '') {
+            alert('Preencha os campos corretamente');
+            event.preventDefault(); 
+            return;
+        }
+
+        if (itemValor.value < 0) {
+            alert('Valor indevido');
+            event.preventDefault(); 
+            return;
+        }
+
+    }
+});
+
+
+
+
+function calcularTotal() {
+    let total = listaDeCompras.reduce((acc, compra) => {
+        return acc + parseFloat(compra.valor) || 0; 
+    }, 0);
+     exibirValorTotal.textContent = `Total: R$ ${total.toFixed(2)}`; 
 }
